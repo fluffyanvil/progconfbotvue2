7f6a9a9160f2d32e1fea4901c246b8eb57ccd049
Sergey <sergeygarnov1991@gmail.com>
2018-03-12 23:10:20 +0300

    server

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+var history = require('connect-history-api-fallback');
+
+app = express();
+app.use(history({
+    rewrites: [
+      { from: "/chats/:chatId", to: "#/chats/:chatId"}
+    ]
+  }));
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
