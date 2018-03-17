a22393d54af7088ce3c3ebf2cb1f7e85c2162ab3
Sergey <sergeygarnov1991@gmail.com>
2018-03-17 21:19:12 +0300

    improvements

--- src/components/ChartAppComponent.vue
+++ src/components/ChartAppComponent.vue
@@ -1,95 +1,86 @@
 <template>
   <div>
-  <div class="ui two column grid cards">
-    <div class="card">
-      <div class="content">
-        <div class="ui statistic">
-          <div class="value">
-            {{total.messagesTotal}}
-          </div>
-          <div class="label">
-            Total messages
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="content">
-        <div class="ui statistic">
-          <div class="value">
-            {{total.stickersTotal}}
-          </div>
-          <div class="label">
-            Total stickers
+    <div class="ui two column grid cards">
+      <div class="card">
+        <div class="content">
+          <div class="ui statistic">
+            <div class="value">
+              {{total.messagesTotal}}
+            </div>
+            <div class="label">
+              Total messages
+            </div>
           </div>
         </div>
       </div>
-    </div>
-</div>
-
-<div class="ui three column grid cards">
-    <div class="card">
-      <div class="content">
-        <div class="ui statistic">
-          <div class="value">
-            {{today.todayMessagesTotal}}
-          </div>
-          <div class="label">
-            Today messages
-          </div>
-        </div>
-        <div class="description">
-          {{today.messagesDirection}}{{today.messagePercentage}}% of {{today.yesterdayMessagesTotal}} yesterday
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="content">
-        <div class="ui statistic">
-          <div class="value">
-            {{today.todayStickersTotal}}
-          </div>
-          <div class="label">
-            Today stickers
-          </div>
-        </div>
-        <div class="description">
-          {{today.stickersDirection}}{{today.stickerPercentage}}% of {{today.yesterdayStickersTotal}} yesterday
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="content">
-        <div class="header">5 last joined users:</div>
-        <div class="description">          
-          <div class="left aligned column">
-            <div class="ui vertical fluid menu" v-for="user in lastUsers" v-bind:key="user.firstName">
-              <div class="header item">
-              {{ user.firstName }}
-              </div>              
+      <div class="card">
+        <div class="content">
+          <div class="ui statistic">
+            <div class="value">
+              {{total.stickersTotal}}
+            </div>
+            <div class="label">
+              Total stickers
             </div>
           </div>
         </div>
       </div>
     </div>
-</div>
 
-<!-- 
+    <div class="ui three column grid cards">
+      <div class="card">
+        <div class="content">
+          <div class="ui statistic">
+            <div class="value">
+              {{today.todayMessagesTotal}}
+            </div>
+            <div class="label">
+              Today messages
+            </div>
+          </div>
+          <div class="description">
+            {{today.messagesDirection}}{{today.messagePercentage}}% of {{today.yesterdayMessagesTotal}} yesterday
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <div class="ui statistic">
+            <div class="value">
+              {{today.todayStickersTotal}}
+            </div>
+            <div class="label">
+              Today stickers
+            </div>
+          </div>
+          <div class="description">
+            {{today.stickersDirection}}{{today.stickerPercentage}}% of {{today.yesterdayStickersTotal}} yesterday
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="content">
+          <div class="header">5 last joined users:</div>
+          <div class="description">          
+            <div class="left aligned column">
+              <div class="ui vertical fluid menu" v-for="user in lastUsers" v-bind:key="user.firstName">
+                <div class="header item">
+                {{ user.firstName }} <a v-bind:href="'https://t.me/'+ user.username" v-show="user.username">@{{ user.username }}</a>
+                </div>              
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-
-
- 
-
-<div class="ui one column cards grid">
-  
-</div>   -->
-  <div class="ui one column cards grid">
-    <div class="card fluid card">
+    <div class="ui one column cards grid">
+      <div class="card fluid card">
         <div class="content">
           <vue-tabs active-tab-color="#175" 
-                active-text-color="white"
-                type="pills"
-                centered>
+                    active-text-color="white"
+                    type="pills"
+                    centered>
             <v-tab title="Daily activity">
               <div class="ui one column cards grid">
                   <chart  label='Messages'
@@ -100,40 +91,34 @@
                           border-width='1'></chart>
               </div>
               <div class="ui one column cards grid">
-                  <chart label='Stickers'
+                  <chart  label='Stickers'
                           api-url='http://progconfbot.herokuapp.com/api/s/'
                           chart-id="stickers"
                           background-color='rgba(152, 99, 132, 0.2)'
                           border-color='rgba(152,99,132,1)'
                           border-width=1></chart>
-              </div>
-              
+              </div>            
             </v-tab>
 
             <v-tab title="User activity">
               <div class="ui one column cards grid">
-                <bar label='Messages'
-                              api-url='http://progconfbot.herokuapp.com/api/top/m/'
-                              chart-id="top-messages"></bar>
+                <bar  label='Messages'
+                      api-url='http://progconfbot.herokuapp.com/api/top/m/'
+                      chart-id="top-messages"></bar>
               </div>              
 
               <div class="ui one column cards grid">
-                <bar label='Stickers'
-                          api-url='http://progconfbot.herokuapp.com/api/top/s/'
-                          chart-id="top-stickers"></bar>
+                <bar  label='Stickers'
+                      api-url='http://progconfbot.herokuapp.com/api/top/s/'
+                      chart-id="top-stickers"></bar>
               </div>              
             </v-tab>
           </vue-tabs>
         </div>
       </div>    
-  </div>
-    
-
-    
+    </div>
   </div>
 </template>
-
-
 
 <script>
 import bar from './BarChartComponent.vue'
@@ -163,33 +148,33 @@ export default {
   },
 
   methods: {
-      updateToday: function () {
-          axios
-              .get('http://progconfbot.herokuapp.com/api/today/' + this.chatId)
-              .then(({data}) => {
-                this.today = data;
-              })
-              .catch((err) => {console.log(err)});
-      },
-      updateTotal: function () {
-          axios
-              .get('http://progconfbot.herokuapp.com/api/total/' + this.chatId)
-              .then(({data}) => {
-                this.total = data;
-              }).catch((err) => {console.log(err)});
-      },
-      updateLastUsers: function() {
-          axios
-              .get('http://progconfbot.herokuapp.com/api/users/last/' + this.chatId)
-              .then(({data}) => {
-              this.lastUsers = data;
-              }).catch((err) => {console.log(err)});
-      }
+    updateToday: function () {
+        axios
+            .get('http://progconfbot.herokuapp.com/api/today/' + this.chatId)
+            .then(({data}) => {
+              this.today = data;
+            })
+            .catch((err) => {console.log(err)});
+    },
+    updateTotal: function () {
+        axios
+            .get('http://progconfbot.herokuapp.com/api/total/' + this.chatId)
+            .then(({data}) => {
+              this.total = data;
+            }).catch((err) => {console.log(err)});
+    },
+    updateLastUsers: function() {
+        axios
+            .get('http://progconfbot.herokuapp.com/api/users/last/' + this.chatId)
+            .then(({data}) => {
+            this.lastUsers = data;
+            }).catch((err) => {console.log(err)});
+    }
   },
   created: function(){
-      this.updateToday();
-      this.updateTotal();
-      this.updateLastUsers();
+    this.updateToday();
+    this.updateTotal();
+    this.updateLastUsers();
   },
   mounted: function() {
 
